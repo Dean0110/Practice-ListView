1d80aed9db5ad1182aa409c0fe24fdcd2d36efae
Dean0110 <wang904183923@163.com>
2020-03-17 15:03:48 +0800

    um

--- app/src/main/java/com/example/practice_listview/MainActivity.java
+++ app/src/main/java/com/example/practice_listview/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.practice_listview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] arrayDemo={
+                "20201744",
+                "20201745",
+                "20201746",
+                "20201747",
+                "20201748"
+        };
+
+        new ArrayAdapter(this,R.layout.item,R.id.item_text);
     }
 }
